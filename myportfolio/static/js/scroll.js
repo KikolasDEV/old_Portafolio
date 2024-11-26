6a08e8fe8f85e73b10496683309393e83c913920
KikolasDEV <kikoamarillo94@gmail.com>
2024-11-26 15:45:15 +0100

    Navbar checked

--- myportfolio/static/js/scroll.js
+++ myportfolio/static/js/scroll.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-        const navbar = document.querySelector(".navbar");
-        window.addEventListener("scroll", function () {
-            if (window.scrollY > 50) {
-                navbar.classList.remove("navbar-top");
-                navbar.classList.add("navbar-scrolled");
-            } else {
-                navbar.classList.add("navbar-top");
-                navbar.classList.remove("navbar-scrolled");
-            }
-        });
+    const navbar = document.querySelector(".navbar");
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 50) {
+            navbar.classList.remove("navbar-top");
+            navbar.classList.add("navbar-scrolled");
+        } else {
+            navbar.classList.add("navbar-top");
+            navbar.classList.remove("navbar-scrolled");
+        }
     });
+});
