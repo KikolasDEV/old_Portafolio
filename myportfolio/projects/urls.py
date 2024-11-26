e451483a3fc90913cbd9854dee680a26a0ebed4d
KikolasDEV <kikoamarillo94@gmail.com>
2024-11-26 16:53:18 +0100

    Contacto checked

--- myportfolio/projects/urls.py
+++ myportfolio/projects/urls.py
@@ -3,7 +3,6 @@ from projects import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('contact/', views.contact, name='contact'),
     path('backend/', views.backend_projects, name='backend_projects'),
     path('websites/', views.websites_projects, name='websites_projects'),
     path('games/', views.games_projects, name='games_projects'),
--- myportfolio/projects/views.py
+++ myportfolio/projects/views.py
@@ -6,9 +6,6 @@ import os
 
 # Create your views here.
 def index(request):
-    return render(request, 'projects/index.html')
-
-def contact(request):
     if request.method == "POST":
         form = ContactForm(request.POST)
         if form.is_valid():
@@ -23,10 +20,10 @@ def contact(request):
                 [os.getenv('MAIL_RECIPIENT')],
             )
             messages.success(request, 'Mensaje enviado. ¡Gracias por contactarme!')
-            return redirect('contact')
+            return redirect('index')
     else:
         form = ContactForm()
-    return render(request, 'projects/contact.html', {'form': form})
+    return render(request, 'projects/index.html', {'form': form})
 
 def backend_projects(request):
     return render(request, 'projects/backend.html')
